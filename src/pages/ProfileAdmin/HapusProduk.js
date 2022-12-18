245863045ed5c4393b97b944761b9710475a5c17
aryadaulat <arya.118140128@student.itera.ac.id>
2022-12-18 13:48:41 +0700

    update Hapus Produk

--- src/pages/ProfileAdmin/HapusProduk.js
+++ src/pages/ProfileAdmin/HapusProduk.js
@@ -1,11 +1,60 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import ComButton from '../../common/ComButton';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 
 const HapusProduk = () => {
   const navigation = useNavigation();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchdata() {
+      const subscriber = await firestore()
+        .collection('Produk')
+        .get()
+        .then(querySnapshot => {
+          var simpandata = [];
+          querySnapshot.forEach(snapshot => {
+            let datas = snapshot.data();
+            let createdata = {
+              key: snapshot.id,
+              berat: datas.berat,
+              harga: datas.harga,
+              image: datas.image,
+              jenis: datas.jenis,
+              kategori: datas.kategori,
+              nama: datas.nama,
+              ukuran: datas.ukuran,
+            };
+            // datas.push({key: snapshot.id});
+            simpandata = [...simpandata, createdata];
+          });
+          setData(simpandata);
+        });
+    }
+    fetchdata();
+  }, []);
+
+  const deletedata = key => {
+    firestore()
+      .collection('Produk')
+      .doc(key)
+      .delete()
+      .then(() => {
+        Alert.alert('Produk Telah Di Delet');
+      });
+  };
   return (
     <SafeAreaView style={{flex: 1}}>
       <View
@@ -16,54 +65,78 @@ const HapusProduk = () => {
           width: '70%',
           height: 100,
         }}>
-            <TouchableOpacity onPress={()=>{navigation.navigate('ProfileAdmin')}}>
-        <Image
-          source={require('../../images/arrow.png')}
-          style={{height: 30, width: 30}}
-          
-        />
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('ProfileAdmin');
+          }}>
+          <Image
+            source={require('../../images/arrow.png')}
+            style={{height: 30, width: 30}}
+          />
         </TouchableOpacity>
         <Text style={{fontSize: 24, fontWeight: 'bold', color: '#000'}}>
-          Hapus Produk 
+          Hapus Produk
         </Text>
       </View>
-      <View style={styles.content}>
-        <Image source={require('../../images/Jas_Lab.png')} />
-        <View>
-          <Text style={styles.txtNama}>Jas Lab Lengan Pendek {'\n'}</Text>
-          <Text style={styles.txt}>Harga : {'Rp 125.000,-'} </Text>
-        </View>
-        <TouchableOpacity>
-          <Image source={require('../../images/Delete.png')} />
-        </TouchableOpacity>
-      </View>
-      <View style={{position: 'absolute',bottom:0, height:150, width:'100%', alignItems:'center'}}>
-        <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'80%', height:40}}>
-        </View>
-        <ComButton title={'Simpan'} onPress={()=>{navigation.navigate('ProfileAdmin')}} bgColor={'#f3c10d'} textColor={'#fff'}/>
-
-
+      <ScrollView style={{marginBottom: 80}}>
+        {data != null &&
+          data.map(datas => (
+            <View style={styles.content} key={datas.key}>
+              <Image source={{uri: datas.image}} style={styles.images} />
+              <View>
+                <Text style={styles.txtNama}>{datas.nama}</Text>
+                <Text style={styles.txt}>Harga : {datas.harga} </Text>
+              </View>
+              <TouchableOpacity onPress={() => deletedata(datas.key)}>
+                <Image source={require('../../images/Delete.png')} />
+              </TouchableOpacity>
+            </View>
+          ))}
+      </ScrollView>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          height: 150,
+          width: '100%',
+          alignItems: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '80%',
+            height: 40,
+          }}></View>
+        <ComButton
+          title={'Simpan'}
+          onPress={() => console.log(data)}
+          bgColor={'#f3c10d'}
+          textColor={'#fff'}
+        />
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  content:{
+  content: {
     flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-          width: '90%',
-          marginLeft: '5%',
-          backgroundColor: '#fff',
-          height: 130,
-          paddingVertical: 10,
-          shadowColor: '#000',
-          shadowOffset: {width: 0, height: 5},
-          shadowOpacity: 1,
-          shadowRadius: 5,
-          elevation: 10,
-          borderRadius:20
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: '90%',
+    marginLeft: '5%',
+    backgroundColor: '#fff',
+    height: 130,
+    paddingVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 5},
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    elevation: 10,
+    borderRadius: 20,
+    marginBottom: 20,
   },
   txtNama: {
     fontSize: 14,
@@ -76,7 +149,10 @@ const styles = StyleSheet.create({
     color: '#000',
     fontWeight: '600',
   },
-
+  images: {
+    width: 80,
+    height: 80,
+  },
 });
 
-export default HapusProduk
+export default HapusProduk;
